34b8ef5b8d0b48db332b69eb0f744ca3f89146ba
Maks <fray09@inbox.ru>
2024-05-11 00:47:21 +0600

    Edit models and add some fields

--- book_app/models.py
+++ book_app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=70)
     rating = models.IntegerField()
-    author = models.CharField(max_length=30)
+    author = models.CharField(max_length=100, null=True)
+    is_best_selling = models.BooleanField()
 
     def __str__(self):
         return f"{self.name} - {self.rating}%"
