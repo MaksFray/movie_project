17ac1f3386895c3848bc8c1aa9437939ba51a070
Maks <fray09@inbox.ru>
2024-05-11 00:10:44 +0600

    Add str to models

--- book_app/models.py
+++ book_app/models.py
@@ -6,3 +6,6 @@ class Book(models.Model):
     name = models.CharField(max_length=70)
     rating = models.IntegerField()
     author = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} - {self.rating}%"
