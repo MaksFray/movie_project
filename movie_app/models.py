cd7023625d2310467365a107569142aba2554665
Maks <fray09@inbox.ru>
2024-05-11 00:00:36 +0600

    Add str to models

--- movie_app/models.py
+++ movie_app/models.py
@@ -6,3 +6,6 @@ class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     author = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} by {self.author} - {self.rating}%"
