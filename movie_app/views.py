acd77f3d06eefd8a1f199c9f19b43dfd7a52c041
Maks <fray09@inbox.ru>
2024-05-12 02:57:52 +0600

    Add view for all movies

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 
 def show_all_movies(request):
-    pass
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies,
+    })
