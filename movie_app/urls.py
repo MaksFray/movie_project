0599ae751fc6a9fcd0c671270957c8621804d1d2
Maks <fray09@inbox.ru>
2024-05-21 23:22:55 +0600

    Add ru lang and admin view

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+admin.site.site_header = "My admin panel"
+admin.site.index_title = "My super admin panel"
+
 urlpatterns = [
     path('', views.show_all_movies),
     path('<slug:slug_movie>', views.show_movie, name='movie_detail'),
