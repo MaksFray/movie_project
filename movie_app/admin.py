8521b4cb7102f9a462fea1a9c189a9fcb09f8025
Maks <fray09@inbox.ru>
2024-05-23 23:57:37 +0600

    Add decorator for admin

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from . models import Movie
 # Register your models here.
 
+@admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'rating']
-
-admin.site.register(Movie, MovieAdmin)
