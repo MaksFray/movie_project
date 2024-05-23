a4738fbce18a84eda3fad5dc029eb066e385fa7c
Maks <fray09@inbox.ru>
2024-05-23 23:55:34 +0600

    Add admin display for Movie

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from . models import Movie
 # Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'rating']
+
+admin.site.register(Movie, MovieAdmin)
