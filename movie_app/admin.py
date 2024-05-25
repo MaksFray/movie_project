fc80bcd91e49ee92d9d391eddd70b17d996fb8d9
Maks <fray09@inbox.ru>
2024-05-26 04:05:10 +0600

    Add rating_status for admin movie

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,7 +4,15 @@ from . models import Movie
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'author', 'rating', 'year']
+    list_display = ['name', 'author', 'rating', 'year', 'rating_status']
     list_editable = ['author', 'year']
     ordering = ['-rating', 'name']
     list_per_page = 10
+    @admin.display(ordering='rating', description='Status')
+    def rating_status(self, movie):
+        if movie.rating < 50:
+            return "Bad"
+        elif 50 <= movie.rating < 80:
+            return "Good"
+        else:
+            return "Fascinating"
