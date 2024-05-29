56f376920d95bdb14f924388e474989d88029e3e
Maks <fray09@inbox.ru>
2024-05-29 11:38:31 +0600

    Add actions for admin movie

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import QuerySet
 from . models import Movie
 # Register your models here.
 
@@ -8,6 +9,7 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ['author', 'year', 'budget', 'currency']
     ordering = ['-rating', 'name']
     list_per_page = 10
+    actions = ['set_rubles', 'set_euro',]
     @admin.display(ordering='rating', description='Status')
     def rating_status(self, movie):
         if movie.rating < 50:
@@ -16,3 +18,16 @@ class MovieAdmin(admin.ModelAdmin):
             return "Good"
         else:
             return "Fascinating"
+
+    @admin.action(description="Set currency to rubles")
+    def set_rubles(self, request, qs: QuerySet):
+        qs.update(currency=Movie.RUB)
+
+    @admin.action(description="Set currency to euro")
+    def set_euro(self, request, qs: QuerySet):
+        count_updates = qs.update(currency=Movie.RUB)
+        self.message_user(
+            request,
+            f'{count_updates} carts was updated',
+            messages.ERROR
+        )
